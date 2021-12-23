48d638ba0c311a578ce9a5627eff2c32719495a6
renanvcb <renanvcb@gmail.com>
2021-12-23 15:41:42 -0300

    test specs for AuthenticateUserUseCase completed

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,13 +1,23 @@
+import jwt from "jsonwebtoken";
+
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { ICreateUserDTO } from "../createUser/ICreateUserDTO";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+import { User } from "../../entities/User";
+import authConfig from '../../../../config/auth';
+import { AppError } from "../../../../shared/errors/AppError";
 
 let createUserUseCase: CreateUserUseCase;
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let inMemoryUsersRepository: InMemoryUsersRepository;
 
 describe("Authenticate User Use Case", () => {
+  interface ITokenUser {
+    user: User,
+    token: string,
+  }
+
   const testUSerData: ICreateUserDTO = {
     name: "John Doe",
     email: "john.doe@mail.com",
@@ -28,6 +38,32 @@ describe("Authenticate User Use Case", () => {
       password: testUSerData.password,
     });
 
-    console.log(user, token);
+    const decodedToken = jwt.verify(token, authConfig.jwt.secret) as ITokenUser;
+
+    expect(user).toHaveProperty("id");
+    expect(user).not.toHaveProperty("password");
+    expect(user.name).toEqual(testUSerData.name);
+    expect(user.email).toEqual(testUSerData.email);
+    expect(decodedToken.user).toHaveProperty("id");
+    expect(decodedToken.user).toHaveProperty("password");
+    expect(decodedToken.user.name).toEqual(testUSerData.name);
+    expect(decodedToken.user.email).toEqual(testUSerData.email);
+  });
+
+  it("should not be able to authenticate an user that does not exist", async () => {
+    await expect(async () => await authenticateUserUseCase.execute({
+      email: testUSerData.email,
+      password: testUSerData.password,
+    })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate an user with incorrect password", async () => {
+    await createUserUseCase.execute(testUSerData);
+
+    await expect(async () => await authenticateUserUseCase.execute({
+      email: testUSerData.email,
+      password: "incorrect password",
+    })).rejects.toBeInstanceOf(AppError);
   });
 });
